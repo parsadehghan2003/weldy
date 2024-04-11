1c52272bfa9982ac80916ffe655cda3940ecae6f
parsadehghan <parsadehghan2003@gmail.com>
2024-04-11 17:59:41 +0330

    Initial commit

---
+++ app/src/main/java/com/ftpd/weldy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ftpd.weldy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
